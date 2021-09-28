a91ff1121dad0e6da1ea89951814fffcb7c45b1a
tordar <tordar.tommervik@gmail.com>
2021-09-28 14:53:18 +0200

    added dev server

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 from flask import Flask, render_template
 
-application = Flask(__name__)
+
 
 x = 'Hello world, from flask'
 
@@ -10,10 +10,16 @@ print('Hello from python')
 a=np.zeros(4)
 print(a)
 
-@application.route('/')
-def homepage():
-    return render_template('index.html', y=x)
+
+def create_app():
+    application = Flask(__name__)
+    @application.route('/')
+    def homepage():
+        return render_template('index.html', y=x)
+
+    return application
 
 
-
-application.run()
+if __name__ == "__main__":
+    application = create_app()
+    application.run(host='0.0.0.0', debug=True)
